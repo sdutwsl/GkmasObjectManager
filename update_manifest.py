12b9f672581384a5d7ffe5c6a82b5f2c77e69a1a
Ziyuan Chen <ziyuan.chen@columbia.edu>
2026-05-31 21:55:14 -0400

    (DRAFT) [Refactor] Move wayback_build_index into update_manifest

--- update_manifest.py
+++ update_manifest.py
@@ -10,9 +10,91 @@ import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
-from GkmasObjectManager import fetch
+from tqdm import tqdm
+
+from GkmasObjectManager import GkmasManifest, fetch
 from GkmasObjectManager.const import WAYBACK_COMMITS_DATABASE_LOCAL
 from GkmasObjectManager.utils import _json_dump, _json_load
+
+
+def fetch_one_manifest(revision: int, commit_hash: str, prog: tqdm) -> GkmasManifest:
+
+    manifest = fetch(this_revision=revision, _hash=commit_hash)
+    prog.update(1)
+    return manifest
+
+
+async def fetch_all_manifests(commits: dict[str, str]) -> list[GkmasManifest]:
+
+    with tqdm(total=len(commits), desc="Fetching manifests") as prog:
+        return await asyncio.gather(
+            *[
+                asyncio.to_thread(fetch_one_manifest, int(revision), commit_hash, prog)
+                for revision, commit_hash in commits.items()
+            ]
+        )
+
+
+def sanitize_canon_repr(canon_repr: dict, revision: int) -> str:
+    return "|".join(
+        [
+            f"{revision:04d}",
+            canon_repr["objectName"],
+            canon_repr["md5"],
+            str(canon_repr["size"]),
+            ",".join(map(str, canon_repr.get("dependencies", []))),
+        ]
+    )
+
+
+def append_index(index: dict, manifest: GkmasManifest) -> None:
+
+    for obj in manifest.assetbundles:
+        ab_id = index["ab_id_lookup"][obj.id]
+        index["assetBundleList"][ab_id]["history"].append(
+            sanitize_canon_repr(obj.canon_repr, manifest.revision.this)
+        )
+
+    for obj in manifest.resources:
+        res_id = index["res_id_lookup"][obj.id]
+        index["resourceList"][res_id]["history"].append(
+            sanitize_canon_repr(obj.canon_repr, manifest.revision.this)
+        )
+
+
+def _rebuild_index():
+
+    commits = _json_load(WAYBACK_COMMITS_DATABASE_LOCAL)
+    manifests = asyncio.run(fetch_all_manifests(commits))
+
+    index = {
+        "latest_revision": manifests[-1].revision.canon_repr,
+        "assetBundleList": [
+            {"id": obj.id, "name": obj.name, "history": []}
+            for obj in manifests[-1].assetbundles
+        ],
+        "resourceList": [
+            {"id": obj.id, "name": obj.name, "history": []}
+            for obj in manifests[-1].resources
+        ],
+        "ab_id_lookup": {
+            obj.id: idx for idx, obj in enumerate(manifests[-1].assetbundles)
+        },
+        "res_id_lookup": {
+            obj.id: idx for idx, obj in enumerate(manifests[-1].resources)
+        },
+        "urlFormat": manifests[-1].urlformat,
+    }
+
+    for i in tqdm(range(len(manifests)), desc="Building index"):
+        if i == 0:
+            append_index(index, manifests[i])
+        else:
+            append_index(index, manifests[i] - manifests[i - 1])
+
+    del index["ab_id_lookup"]
+    del index["res_id_lookup"]
+    _json_dump(index, "wayback_index.json")
 
 
 def fetch_one(path: Path, rev: int, pc: bool):
